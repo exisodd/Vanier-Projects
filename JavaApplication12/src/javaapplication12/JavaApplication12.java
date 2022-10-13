63aeba8755f0293469397e1268c1095d3987b2d8
exisodd <ethantran004@yahoo.ca>
2022-10-13 11:30:08 -0400

    Test commit

--- JavaApplication12/src/javaapplication12/JavaApplication12.java
+++ JavaApplication12/src/javaapplication12/JavaApplication12.java
@@ -16,6 +16,6 @@ public class JavaApplication12 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        System.out.println("Hello, world!");
     }
 }
