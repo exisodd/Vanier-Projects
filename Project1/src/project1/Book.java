14277f958e2c733ee48a48d032beb2fbb29d925a
exisodd <ethantran004@yahoo.ca>
2022-11-30 22:04:49 -0500

    Project 1

--- Project1/src/project1/Book.java
+++ Project1/src/project1/Book.java
@@ -1,6 +1,7 @@
 package project1;
 
-import java.util.Objects;
+import java.util.Arrays;
+import java.util.List;
 
 public class Book {
 
@@ -13,18 +14,18 @@ public class Book {
     public Book() {
         // TODO ??EMPTY MEMBERS OR FILLED NO_TITLE???
         title = "";
-        author = "";
+        author = "Unknown";
         price = 0;
-        publisher = "";
-        isbn = "";
+        publisher = "To be confirmed";
+        isbn = "0-1234-5678-9";
     }
 
     public Book(String title) {
         this.title = title;
-        this.author = "";
+        this.author = "Unknown";
         this.price = 0;
-        this.publisher = "";
-        this.isbn = "";
+        this.publisher = "To be confirmed";
+        this.isbn = "0-1234-5678-9";
     }
 
     public Book(String title, String author, double price, String publisher, String isbn) {
@@ -44,16 +45,43 @@ public class Book {
     }
 
     public int checkIsbnStatus() {
-        int length = isbn.replace("-", "").length();
+        int length = isbn.length();
         int result = -1;
-        if (length == 10 && isbn.charAt(1) == '-' &&
-            isbn.charAt(6) == '-' && isbn.charAt(11) == '-') {
+        List<Integer> dash10 = Arrays.asList(1, 6, 11);
+        List<Integer> dash13 = Arrays.asList(3, 5, 10, 15);
+
+        // Validate 10-digit isbn
+        if (length == 13) {
+            // Validate digit validity
+            for (int i = 0; i < length; i++) {
+                // Char not digit, so check if dash and proper index location
+                if (!Character.isDigit(isbn.charAt(i))) {
+                    if (isbn.charAt(i) != '-') {
+                        return -1;
+                    }
+                    if (!dash10.contains(i)) {
+                        return -1;
+                    }
+                }
+            }
             result = 0;
-        } else if (length == 13 && isbn.charAt(3) == '-' &&
-                   isbn.charAt(5) == '-' && isbn.charAt(10) == '-' &&
-                   isbn.charAt(15) == '-') {
+        // Validate 13-digit isbn
+        } else if (length == 17) {
+            // Validate digit validity
+            for (int i = 0; i < length; i++) {
+                // Char not digit, so check if dash and proper index location
+                if (!Character.isDigit(isbn.charAt(i))) {
+                    if (isbn.charAt(i) != '-') {
+                        return -1;
+                    }
+                    if (!dash13.contains(i)) {
+                        return -1;
+                    }
+                }
+            }
             result = 1;
         }
+
         return result;
     }
 
@@ -66,13 +94,15 @@ public class Book {
         // Convert each word in author to title case
         for (String word: author.split(" ")) {
             finalAuthor.append(Character.toTitleCase(word.charAt(0)))
-                       .append(word.substring(1).toLowerCase());
+                       .append(word.substring(1).toLowerCase())
+                       .append(" ");
         }
 
         // Convert each word in title to title case
         for (String word: title.split(" ")) {
             finalTitle.append(Character.toTitleCase(word.charAt(0)))
-                      .append(word.substring(1).toLowerCase());
+                      .append(word.substring(1).toLowerCase())
+                      .append(" ");
         }
 
         // Update data members
@@ -139,5 +169,11 @@ public class Book {
 
     public void setIsbn(String isbn) {
         this.isbn = isbn;
+    }
+
+    public static void main(String[] args) {
+        Book myBook = new Book("My l i jeff", "stuff like f", 0, "To be confirmed", "978-4-6978-9706-6");
+        myBook.toTitleCase();
+        System.out.println(myBook.toString());
     }
 }
--- Project1/src/project1/Library.java
+++ Project1/src/project1/Library.java
@@ -18,7 +18,7 @@ public class Library {
     }
 
     public ArrayList<Book> searchBook(String keyword) {
-        ArrayList<Book> result = new ArrayList<Book>();
+        ArrayList<Book> result = new ArrayList<>();
         for (Book book: books) {
             if (book.getAuthor().contains(keyword) || book.getTitle().contains(keyword)) {
                 result.add(book);
@@ -28,15 +28,45 @@ public class Library {
     }
 
     public boolean equals(Library library) {
-        return library.books.equals(books);
+        // Check if arraylists are of equal length
+        if (books.size() != library.books.size()) {
+            return false;
+        }
+
+        // If so, validate equality of each book
+        for (int i = 0; i < books.size(); i++) {
+            if (!books.get(i).equals(library.books.get(i))) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
         for (Book book: books) {
-            result.append(books.toString());
+            result.append(book.toString()).append("===============================\n");
         }
         return result.toString();
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Book> list = new ArrayList<>();
+        Book myBook = new Book("My jeff", "stuff like f", 0, "To be confirmed", "978-4-6978-9706-6");
+        Book book2 = new Book("Book 2", "More stuff", 0, "To be confirmed", "978-4-6978-9706-6");
+        list.add(myBook);
+        list.add(book2);
+
+        ArrayList<Book> list2 = new ArrayList<>();
+        Book book1 = new Book("My jeff", "stuff like f", 0, "To be confirmed", "978-4-6978-9706-6");
+        Book book3 = new Book("Book 2", "More stuff", 0, "To be confirmed", "978-4-6978-9706-6");
+        list2.add(book1);
+        list2.add(book3);
+
+        Library library = new Library(list);
+        Library library2 = new Library(list2);
+        System.out.println(library.equals(library2));
     }
 }
