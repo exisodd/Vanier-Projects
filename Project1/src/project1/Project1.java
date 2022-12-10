42362ecc3d52c8a605c915c7d44a576b3636eec0
exisodd <ethantran004@yahoo.ca>
2022-12-09 23:04:38 -0500

    Project 1

--- Project1/src/project1/Project1.java
+++ Project1/src/project1/Project1.java
@@ -16,11 +16,18 @@ public class Project1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ArrayList<Book> bookList = new ArrayList<>();
+        ArrayList<Book> bookList1 = new ArrayList<>();
+        ArrayList<Book> bookList2 = new ArrayList<>();
+
         Book myBook1 = new Book("How to speak chinese", "your name",
-                9.99, "Warner brothers", "0-7291-9466-3");;
+                9.99, "Warner brothers", "0-7291-9466-3");
         Book myBook2 = new Book("Funny tunes", "jerry stewart",
-                3, "To be confirmed", "978-4-2513-9706-6");;
+                3, "To be confirmed", "978-4-2513-9706-6");
+
+        Book myBook3 = new Book("How to speak chinese", "your name",
+                9.99, "Warner brothers", "0-7291-9466-3");
+        Book myBook4 = new Book("Funny tunes", "jerry stewart",
+                3, "To be confirmed", "978-4-2513-9706-6");
 
         try {
             myBook1.setPrice(-3);
@@ -28,11 +35,14 @@ public class Project1 {
             e.printStackTrace();
         }
 
-        bookList.add(myBook1);
-        bookList.add(myBook2);
-
-        Library myLibrary = new Library(bookList);
-        System.out.println(myLibrary);
+        bookList1.add(myBook1);
+        bookList1.add(myBook2);
+        bookList2.add(myBook3);
+        bookList2.add(myBook4);
+        Library myLibrary1 = new Library(bookList1);
+        Library myLibrary2 = new Library(bookList2);
+        System.out.println(myLibrary1);
+        System.out.println(myLibrary1.equals(myLibrary2));
     }
     
 }
